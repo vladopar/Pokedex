bb974d3a280cc7da2921da5b9bca635032ebfa84
vladopar <parizek.vladimir@gmail.com>
2023-02-14 14:32:55 +0100

    Add placeholder when loading new data for list

--- app/src/main/java/com/parizek/myapplication/presentation/list_screen/PokemonListScreen.kt
+++ app/src/main/java/com/parizek/myapplication/presentation/list_screen/PokemonListScreen.kt
@@ -1,5 +1,10 @@
 package com.parizek.myapplication.presentation.list_screen
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -28,10 +33,15 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
@@ -87,7 +97,7 @@ fun PokemonListScreen(
             horizontalArrangement = Arrangement.spacedBy(12.dp),
             state = listState,
 
-        ) {
+            ) {
             items(
                 pokemons.itemCount,
             ) { index ->
@@ -114,7 +124,8 @@ fun PokemonListScreen(
                                     .align(Alignment.TopStart)
                             ) {
                                 Text(
-                                    text = pokemon?.nameForList?.replaceFirstChar { it.uppercase() } ?: "",
+                                    text = pokemon?.nameForList?.replaceFirstChar { it.uppercase() }
+                                        ?: "",
                                     fontWeight = FontWeight.Bold,
                                     color = almostWhite
                                 )
@@ -127,7 +138,9 @@ fun PokemonListScreen(
                             Image(
                                 painter = painterResource(id = R.drawable.pokeball_png),
                                 contentDescription = null,
-                                colorFilter = ColorFilter.tint(pokemon?.colorsForList?.first() ?: Color.LightGray),
+                                colorFilter = ColorFilter.tint(
+                                    pokemon?.colorsForList?.first() ?: Color.LightGray
+                                ),
                                 modifier = Modifier
                                     .size(200.dp)
                                     .align(Alignment.BottomEnd)
@@ -155,17 +168,41 @@ fun PokemonListScreen(
                 is LoadState.Error -> {
 
                 }
+
                 is LoadState.Loading -> {
                     item {
+                        val currentRotation by remember { mutableStateOf(0f) }
+
+                        val rotation = remember {
+                            Animatable(currentRotation)
+                        }
+                        LaunchedEffect(key1 = true) {
+                            rotation.animateTo(
+                                targetValue = currentRotation + 360f,
+                                animationSpec = infiniteRepeatable(
+                                    animation = tween(3000, easing = FastOutSlowInEasing),
+                                    repeatMode = RepeatMode.Restart
+                                )
+                            )
+                        }
                         Column(
                             horizontalAlignment = Alignment.CenterHorizontally,
                             verticalArrangement = Arrangement.Center,
                             modifier = Modifier.fillMaxSize()
                         ) {
-                            CircularProgressIndicator(color = Color.Black)
+//                            CircularProgressIndicator(color = Color.Black)
+                            Image(
+                                painter = painterResource(id = R.drawable.pokeball_png),
+                                contentDescription = null,
+                                colorFilter = ColorFilter.tint(Color.LightGray),
+                                modifier = Modifier
+                                    .rotate(rotation.value)
+                                    .size(250.dp)
+                            )
                         }
                     }
                 }
+
                 else -> {}
             }
 
@@ -173,20 +210,68 @@ fun PokemonListScreen(
                 is LoadState.Error -> {
 
                 }
+
                 is LoadState.Loading -> {
                     item {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            CircularProgressIndicator(color = Color.Black)
-                        }
+                        PlaceholderCard()
+                    }
+                    item {
+                        PlaceholderCard()
                     }
                 }
+
                 else -> {}
             }
+        }
+    }
+}
+
+@Composable
+fun PlaceholderCard() {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = Color.DarkGray
+        ),
+        modifier = Modifier
+            .height(150.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .align(Alignment.TopStart)
+            ) {
+                Text(
+                    text = "",
+                    fontWeight = FontWeight.Bold,
+                    color = almostWhite
+                )
+                Text(
+                    text = "",
+                    fontWeight = FontWeight.Bold,
+                    color = almostWhite
+                )
+            }
+            Image(
+                painter = painterResource(id = R.drawable.pokeball_png),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Color.LightGray),
+                modifier = Modifier
+                    .size(200.dp)
+                    .align(Alignment.BottomEnd)
+                    .offset(x = 50.dp, y = 20.dp)
+            )
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(Alignment.BottomEnd)
+                    .offset(x = 10.dp, y = 10.dp)
+            )
         }
     }
 }
